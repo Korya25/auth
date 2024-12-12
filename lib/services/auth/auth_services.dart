d904946c6cd2d960cd5722495cf1157372910618
Korya25 <enthrj3@gmail.com>
2024-12-13 00:55:27 +0200

    add loginwithemail auth services

--- lib/services/auth/auth_services.dart
+++ lib/services/auth/auth_services.dart
@@ -2,6 +2,7 @@ import 'package:authapp/helper/snakbar.dart';
 import 'package:authapp/services/auth/auth_error_handler.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class AuthServices {
   // Instance of FirebaseAuth
@@ -35,7 +36,33 @@ class AuthServices {
       return null;
     }
   }
+
   // Login With Email
+  Future<UserCredential?> loginWithEmail({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      final String errorMessage = handleFirebaseAuthError(e.code);
+      if (context.mounted) {
+        customSnackBar(context, errorMessage);
+      }
+      return null;
+    } catch (e) {
+      if (context.mounted) {
+        customSnackBar(
+            context, 'An unexpected error occurred. Please try again.');
+      }
+      return null;
+    }
+  }
 
   // Login With Google
 
