b9ababfcb18cd7f2dd35f2dfcf0c4c62930213ae
Korya25 <enthrj3@gmail.com>
2024-12-13 14:40:40 +0200

    create Home screen  and Custom my drawer

---
+++ lib/views/widgets/my_drawer.dart
@@ -0,0 +1,90 @@
+import 'package:authapp/constanta/app_text_style.dart';
+import 'package:authapp/views/screen/home/settings_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Color.fromARGB(255, 45, 45, 49),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              // Logo
+              DrawerHeader(
+                child: Center(
+                  child: Icon(
+                    Icons.message,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                ),
+              ),
+
+              // Home list title
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text(
+                    "H o m e",
+                    style: AppTextStyle.labelTextFormField,
+                  ),
+                  leading: const Icon(
+                    Icons.home,
+                    color: Colors.white,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+
+              // Settings
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text(
+                    "S E T T I N G S",
+                    style: AppTextStyle.labelTextFormField,
+                  ),
+                  leading: const Icon(
+                    Icons.settings,
+                    color: Colors.white,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return SettingeScreen();
+                      },
+                    ));
+                  },
+                ),
+              ),
+
+              // Logout list tile
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: ListTile(
+              title: Text(
+                "L O G O U T",
+                style: AppTextStyle.labelTextFormField,
+              ),
+              leading: const Icon(
+                Icons.logout,
+                color: Color.fromARGB(255, 255, 19, 2),
+              ),
+              onTap: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
